98e40ffb43b9d834637a6ef85f7fe3b3e020872c
Flav1us <an215@ukr.net>
2019-03-29 20:36:49 +0200

    BandB first try untested

--- 5_year/D5_DifferentialCryptanalysis/src/main/BranchAndBound.java
+++ 5_year/D5_DifferentialCryptanalysis/src/main/BranchAndBound.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
@@ -10,16 +13,37 @@ public class BranchAndBound {
 	static Map<Character, MyEntry> precalc = new ConcurrentHashMap<>();
 	
 	static {
-		precalc.put((char)1, new MyEntry((char)10001, 0.25));
-		precalc.put((char)10001, new MyEntry((char)110011, 0.0625));
+		precalc.put((char)0b1, new MyEntry((char)0b10001, 0.25));
+		precalc.put((char)0b10001, new MyEntry((char)0b110011, 0.0625));
+		precalc.put((char)0b110011, new MyEntry((char)0b1100110000, 0.0625));
+		precalc.put((char)0b1100110000, new MyEntry((char)0b11001100000, 0.0625));
+		precalc.put((char)0b11001100000, new MyEntry((char)0b110000001100000, 0.0625));
+		precalc.put((char)0b110000001100000, new MyEntry((char)0b1010000010100000, 0.0625));
 	}
 	
 	
 	private static class MyEntry {
-		public MyEntry() {};
+		//public MyEntry() {};
 		public MyEntry(char b, double p) {this.beta = b; this.prob = p;}
 		public char beta;
 		public double prob;
+	}
+	
+	private static class Node {
+		char value;
+		double input_prob;//what if several parents? its good
+		List<Node> parents = new ArrayList<>();
+		
+		//next round members with probabilities to get into them
+		Map<Node, Double> children = new TreeMap<>();
+		
+		public Node(char value, Node parent) {
+			this.value = value;
+		}
+
+		public void addChild(Node child, double prob) {
+			children.put(child, prob);
+		}
 	}
 	
 	//ignore this func yet
@@ -50,21 +74,39 @@ public class BranchAndBound {
 		
 	}
 	
-	public static void singleLine(){
-		double p_min = 0.1;
-		char alpha0 = 0b0000_0000_0000_0001;
-		char alpha1 = 0;
-		for(char beta = Character.MIN_VALUE; beta < Character.MAX_VALUE; beta++) {
-			//System.out.println(Integer.valueOf(beta));
-			double p = roundDiffProb(alpha0, beta);
-			if(p >= p_min) {
-				System.out.println(Integer.toBinaryString(beta) + "\t" + p);
-				alpha1 = beta;
-				break;
+	public static void BandB(char start_diff, double minprob, int rounds) {
+		//List<Map<Character, Map<Character, Double>>> diff = new ArrayList<Map<Character, Map<Character, Double>>>();
+		Map<Character, Double>[] omega = new HashMap[7]; //
+		//Node root = new Node(start_diff, null);
+		omega[0].put(start_diff, 1.0);
+		for(int i = 1; i <= 5; i++) {//5 rounds differentials
+			for(Map.Entry<Character, Double> parent : omega[i-1].entrySet()) {
+				for(char beta = Character.MIN_VALUE; beta < Character.MAX_VALUE; beta++) {
+					double prob = roundDiffProb(parent.getKey(), beta);
+					//if(prob >= minprob) {
+						if(omega[i].containsKey(beta)) {
+							omega[i].put(beta, omega[i].get(beta) + parent.getValue()*prob);
+						}
+						else {
+							omega[i].put(beta, parent.getValue()*prob);
+						}
+					//}
+				}
 			}
+			//here bound
+			for(Map.Entry<Character, Double> child : omega[i].entrySet()) {
+				if(child.getValue() < minprob) {
+					omega[i].remove(child);
+				}
+			}
+			
 		}
 		
-		maxRoundDP(alpha1);
+	}
+	
+	public static void singleLine(){
+		
+		maxRoundDP((char)0b110000001100000);
 	}
 	
 	public static double maxRoundDP(char alpha) {
