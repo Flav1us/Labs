cb7d8aa0db7e3d31b32edb7f3c7d9b61617bba64
Flav1us <an215@ukr.net>
2019-03-29 14:26:47 +0200

    round decryption

--- 5_year/D5_DifferentialCryptanalysis/src/main/Heys.java
+++ 5_year/D5_DifferentialCryptanalysis/src/main/Heys.java
@@ -3,11 +3,13 @@ package main;
 public class Heys {
 	
 	//var 8
-	private static int[] S = {	1,		2,		3,		0xE,
+	public static int[] S = {	1,		2,		3,		0xE,
 								6,		0xD,	0xB,	8,
 								0xF, 	0xA, 	0xC, 	5,
 								7, 		9, 		0, 		4};
 
+	
+	public static int[] S_reverse = reverse(S);
 	/*private static int[] S = new int[16]; //test
 	static {
 		for(int i = 0; i < S.length; i++) S[i] = i;
@@ -17,6 +19,14 @@ public class Heys {
 		// TODO Auto-generated constructor stub
 	}
 	
+	private static int[] reverse(int[] s2) {
+		int[] s_reverse = new int[s2.length];
+		for(int i = 0; i <s_reverse.length; i++) {
+			s_reverse[s2[i]] = i;
+		}
+		return s_reverse;
+	}
+
 	public static char encrypt(char m, char[] key) {
 		if(key.length != 7) System.out.println("key length might be wrong");
 		char res = m;
@@ -33,7 +43,7 @@ public class Heys {
 		block ^= round_key;
 		char[] t = new char[4];
 		for(int i = 0; i < 4; i++) {
-			t[i] = (char) (S[block >>> 4*i & 0xF]);
+			t[i] = (char) S[block >>> 4*i & 0xF];
 			t[i] = (char)  (((t[i] & 0b0001) << 3) ^ // 0bABCD -> 0bA000_B000_C000_D000
 							((t[i] & 0b0010) << 6) ^
 							((t[i] & 0b0100) << 9) ^
@@ -41,6 +51,30 @@ public class Heys {
 			t[i] = (char) (t[i] >> 3-i);
 			res ^= t[i];
 		}
+		return res;
+	}
+	
+	
+	
+	public static char decr_round (char block, char round_key) {
+		char permutation = 0;
+		char[] t = new char[4];
+		for(int i = 0; i < 4; i++) {
+			t[i] = (char) (block >>> 4*i & 0xF);
+			t[i] = (char)  (((t[i] & 0b0001) << 3) ^ // 0bABCD -> 0bA000_B000_C000_D000
+							((t[i] & 0b0010) << 6) ^
+							((t[i] & 0b0100) << 9) ^
+							((t[i] & 0b1000) << 12));
+			t[i] = (char) (t[i] >> 3-i);
+			permutation ^= t[i];
+		}
+		//permuted; next S-blocks:
+		char res = 0;
+		for(int i = 0; i < 4; i++) {
+			res ^= (char) (S_reverse[permutation >>> 4*i & 0xF] << 4*i);
+		}
+		//now round key add
+		res ^= round_key;
 		return res;
 	}
 
--- 5_year/D5_DifferentialCryptanalysis/src/test/Test1.java
+++ 5_year/D5_DifferentialCryptanalysis/src/test/Test1.java
@@ -1,21 +1,44 @@
 package test;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.Random;
+
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 
 import main.Heys;
 
 public class Test1 {
 
-	public Test1() {
-		// TODO Auto-generated constructor stub
-	}
 	
 	@Test
+	@Ignore
 	public void test_round() {
 		char m = 0b1100101100101001; //with test S should do 1000_0010_0010_0000
 		char k = 0b0100101101001001;
-		System.out.println(Integer.toBinaryString(Heys.round(m, k)));
+		//System.out.println(Integer.toBinaryString(Heys.round(m, k)));
+		//is correct??
+		assertTrue("1010100010101111".equals(Integer.toBinaryString(Heys.round(m, k))));
 		
+	}
+	
+	@Test
+	@Ignore
+	public void test_reverse() {
+		for(int i = 0; i < Heys.S.length; i++) {
+			assertTrue(Heys.S[Heys.S_reverse[i]] == i);
+		}
+	}
+	
+	@Test
+	public void test_decrypt_block() {
+		Random r = new Random();
+		for(int i = 0; i <20; i++ ) {
+			char block = (char)r.nextInt();
+			char key = (char)r.nextInt();
+			assertTrue(Heys.decr_round(Heys.round(block, key), key) == block);
+		}
 	}
 
 }
